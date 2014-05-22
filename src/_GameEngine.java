98c599ae894cd252d3e459169016408204680a43
insipid2 <blorton@gmail.com>
2014-05-21 20:10:41 -0700

    started state 4, piece selection.  logic error in row selection.  logic error in piece team comparison

--- src/_GameEngine.java
+++ src/_GameEngine.java
@@ -6,14 +6,20 @@ public class _GameEngine {
      * @param args
      */
     public static void main(String[] args) {
-
-        // VARIABLES
+        ///////////////
+        // VARIABLES //
+        ///////////////
         Board gameBoard = null;
         Scanner input = new Scanner(System.in);
         boolean hasPotentialDesire = true;		
 
-        String tempInput = ""; // temporary storage for player input
-        boolean invalidInput = false;
+        String tempInput = "";          // temporary storage for player input
+        boolean invalidInput = false;   // flag for whether there has been valid input
+        int inputRowInt = 0;            // stores row input from player
+        char inputColChar = ' ';        // stores column input from player
+        int arrayRowInt = 0;            // stores row number corresponding to game board array
+        int arrayColInt = 0;            // stores column number corresponding to game board array
+        Piece mySelectedPiece;          // stores the piece the player has chosen to move
 
         // Game State, whose turn is it?
         String colorTurn = "Black";
@@ -31,12 +37,15 @@ public class _GameEngine {
         int gameState = 0;
 
 
-
+        //////////////////////
+        // USER INTERACTION //
+        //////////////////////
         while (hasPotentialDesire) {
         
             // 0 - Program Start
             // Want to start a new game?
             if(gameState == 0){
+                invalidInput = printInvalidInput(invalidInput);
                 System.out.println("How about a nice game of Chess?");
                 System.out.println("Enter 1 for new game, 0 to exit.");
                 tempInput = input.next();
@@ -51,19 +60,16 @@ public class _GameEngine {
                     gameState++;
                 }
                 else{
-                    printInvalidInput();
+                    invalidInput = true;
                 }
             }
             
             // 1 - Beginning of turn
             // Play or concede?
-            if (gameState == 1){
+            if(gameState == 1){
                 clearScreen();
                 gameBoard.display();
-                if(invalidInput){
-                    invalidInput = false;
-                    printInvalidInput();
-                }
+                invalidInput = printInvalidInput(invalidInput);
                 printHeader(colorTurn, gameState);
                 System.out.print("Enter 1 to take turn, or 0 to concede: ");
                 tempInput = input.next();
@@ -82,15 +88,97 @@ public class _GameEngine {
                 }
             }
             
-            if (gameState == 2){
-                clearScreen();
+            // 2 - Piece Selection, Row
+            if(gameState == 2){
+                clearScreen();                
                 gameBoard.display();
+                invalidInput = printInvalidInput(invalidInput);
                 printHeader(colorTurn, gameState);
                 System.out.print("Enter the row number: (0-7) ");
+                tempInput = input.next();
+                
+                try{
+                    inputRowInt = Integer.parseInt(tempInput);
+                }
+                catch(NumberFormatException nfe){
+                    invalidInput = true;
+                    inputRowInt = -1;
+                }
+                
+                if(inputRowInt >= 0 && inputRowInt <= 7){
+                    gameState++;
+                }
+                else{
+                    invalidInput = true;
+                }
+                
+            }
+            
+            // 3 - Piece Selection, Column
+            if(gameState == 3){
+                clearScreen();                
+                gameBoard.display();
+                invalidInput = printInvalidInput(invalidInput);
+                printHeader(colorTurn, gameState);
+                System.out.println("Row " + inputRowInt + " selected");
+                System.out.print("Enter the column letter: (a-h) ");
+                tempInput = input.next();
+                
+                if(tempInput.length() == 1){
+                    switch(tempInput.charAt(0)){
+                        case 'a': case 'b': case 'c': case 'd':
+                        case 'e': case 'f': case 'g': case 'h':
+                            inputColChar = tempInput.charAt(0);
+                            gameState++;
+                        default:
+                            invalidInput = true;
+                    }
+                }
+                else{
+                    invalidInput = true;
+                }                
+            }
+            
+            // 4 - Piece Selection, confirm
+            if(gameState == 4){
+                arrayRowInt = rowConvert(inputRowInt);
+                arrayColInt = colUItoArray(inputColChar);
+                clearScreen();                
+                gameBoard.display();
+                invalidInput = printInvalidInput(invalidInput);
+                printHeader(colorTurn, gameState);
+                System.out.println(gameBoard.getPiece(arrayRowInt, arrayColInt).toString() + " at " + inputRowInt + " - " + inputColChar + " selected");
+                if (isMyPiece(gameBoard, arrayRowInt, arrayColInt, colorTurn)){
+                    System.out.println("Congratulations.  You have successfully chosen one of your pieces.");
+                    System.out.println("Is this the piece you would like to move?");
+                    System.out.println("Enter 1 to confirm piece selection, enter 0 to cancel");
+                    tempInput = input.next();
+                    
+                    if(tempInput.equals("0")){
+                        gameState = gameState - 2;
+                    }
+                    else if(tempInput.equals("1")){
+                        gameState++;
+                        mySelectedPiece = gameBoard.getPiece(arrayRowInt, arrayColInt);                        
+                    }
+                    else{
+                        invalidInput = true;
+                    }
+                }
+                else{
+                    invalidInput = true;
+                    gameState = gameState - 2;
+                }
+            }
+            
+            // 5 - break here to test
+            if(gameState == 5){
+                System.out.println("piece chosen?");
+                System.exit(0);
             }
             
 
-            //System.out.println("           Welcome to Super Battle Chess 2D VII!!!");
+            // System.out.println("           Welcome to Super Battle Chess 2D VII!!!");
 
 
         }
@@ -101,11 +189,60 @@ public class _GameEngine {
 
 
 
+    // check if the space at row, column (array) in the given board contains a piece belonging to specified color
+    private static boolean isMyPiece (Board gameBoard, int row, int col, String sTeam){
+        int iTeam = -1;
+        if (sTeam == "black"){
+            iTeam = 0;
+        }
+        else if (sTeam == "white"){
+            iTeam = 1;
+        }
+        return (iTeam == gameBoard.getPiece(row, col).team);
+    }
     
+    // convert pre-validated row number to/from player input to/from internal array number
+    private static int rowConvert(int input){
+        return 7 - input;
+    }
     
-    //Invalid input, let the player know
-    private static void printInvalidInput(){
-        System.out.println("Ouch, what do you do?");
+    // convert pre-validated column character from player input to internal array number
+    private static int colUItoArray(char input){
+        switch (input){
+            case 'a': return 0;
+            case 'b': return 1;
+            case 'c': return 2;
+            case 'd': return 3;
+            case 'e': return 4;
+            case 'f': return 5;
+            case 'g': return 6;
+            case 'h': return 7;
+            default: return -1;        
+        }
+    }
+    
+ // convert pre-validated column character from internal array number to UI 
+    private static char colArraytoUI(int input){
+        switch (input){
+            case 0: return 'a';
+            case 1: return 'b';
+            case 2: return 'c';
+            case 3: return 'd';
+            case 4: return 'e';
+            case 5: return 'f';
+            case 6: return 'g';
+            case 7: return 'h';
+            default: return ' ';        
+        }
+    }
+    
+    // if input is invalid, let the player know
+    private static boolean printInvalidInput(boolean invalidInput){
+        
+        if(invalidInput){
+            System.out.println("Ouch, what do you do?");            
+        }        
+        return false;
     }
     
     // Displays whose turn it is, and 
